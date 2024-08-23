a3dd901fd4f3fab760d8be2554fd6751958755cd
Milaneso <chiro_lee@hotmail.com>
2024-08-22 21:12:38 -0600

    Delete Ejercicio 1.py

--- Ejercicio 1.py
+++
@@ -1,6 +0,0 @@
-#Escribir un programa que solicite al usuario su nombre y edad, y luego imprima un mensaje de bienvenida
-#que incluya ambos.
-
-nombre = input("¿Cuál es tu nombre? ")
-edad = input("¿Cuántos años tienes? ")
-print("Bienvenido " ,(nombre), "tienes " ,(edad), "años de edad " )
