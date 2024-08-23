942015a3d779a0f85c8209d92cae2027661b96dd
Milaneso <chiro_lee@hotmail.com>
2024-08-22 21:14:35 -0600

    Delete Ejercicio 8.py

--- Ejercicio 8.py
+++
@@ -1,7 +0,0 @@
-#Hacer un script que pida al usuario dos cadenas de texto
-#  y las imprima concatenadas.
-
-cadena1 = input("Introduce la primera cadena de texto: ")
-cadena2 = input("Introduce la segunda cadena de texto: ")
-cadena_concatenada = cadena1 + cadena2
-print("La cadena concatenada es:", cadena_concatenada)
