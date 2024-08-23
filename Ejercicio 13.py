7386fc856f681cef3a5d52ca5c53abec2bb817f3
Milaneso <chiro_lee@hotmail.com>
2024-08-22 21:13:18 -0600

    Delete Ejercicio 13.py

--- Ejercicio 13.py
+++
@@ -1,8 +0,0 @@
-#Hacer un programa que pida al usuario una lista de nombres y los ordene alfabeticamente.
-
-entrada = input("Introduce una lista de nombres separados por comas: ")
-lista_nombres = [nombre.strip() for nombre in entrada.split(',')]
-lista_nombres_ordenada = sorted(lista_nombres)
-print("Lista de nombres ordenada alfabéticamente:")
-for nombre in lista_nombres_ordenada:
-    print(nombre)
