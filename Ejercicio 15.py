91f43babb9911aca96022392cb6cc7e34a1d2e11
Milaneso <chiro_lee@hotmail.com>
2024-08-22 21:13:35 -0600

    Delete Ejercicio 15.py

--- Ejercicio 15.py
+++
@@ -1,6 +0,0 @@
-#Desarrollar un script que pida al usuario una oracion y cuente cuantas palabras tiene.
-
-oracion = input("Introduce una oración: ")
-palabras = oracion.split()
-numero_de_palabras = len(palabras)
-print(f"La oración tiene {numero_de_palabras} palabras.")
