e181875c84ee0f09da0ea9f8435f7318e5149d1c
Milaneso <chiro_lee@hotmail.com>
2024-08-22 21:14:42 -0600

    Delete Ejercicio 9.py

--- Ejercicio 9.py
+++
@@ -1,5 +0,0 @@
-#Crear un programa que pida al usuario su nombre y luego lo imprima invertido.
-
-nombre = input("Introduce tu nombre: ")
-nombre_invertido = nombre[::-1]
-print("Tu nombre invertido es:", nombre_invertido)
